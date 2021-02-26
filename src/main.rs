d1cc32aa0e8ef02a082d92f0331f5c5d9ed06a58
Shadowmeth <47725754+Shadowmeth@users.noreply.github.com>
2021-02-26 14:38:41 +0500

    Create main.rs

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
+
+fn main() {
+    println!("Guess the number!");
+    // random number generation
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+    //println!("The secret number is: {}", secret_number);
+
+    loop {
+        println!("Enter your guess: ");
+
+        let mut guess = String::new();
+
+        // user input
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Error reading input");
+
+        // parse user input into int, although guess exists we can "shadow" it
+        // it allows us to have a single variable instead of creating multiple variables
+        // named guess, guess_str etc...
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => { 
+                println!("You win!");
+                println!("The secret number was {}", secret_number);
+                break;
+            }
+        }
+    }
+}
